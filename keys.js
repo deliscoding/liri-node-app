5d366919c1a5b22387c45853b0bb542dfab246a0
detour35 <danelis19@gmail.com>
2018-03-07 09:18:44 -0500

    added twitter and spotify key and secret info to keys.js

---
+++ keys.js
@@ -0,0 +1,14 @@
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.dJagdXNrG4Aixv5DFfEtRygIe,
+  consumer_secret: process.env.rfX6mfrceL6cx8vCE1SIYaG0FGENIlL6vXhfNCaAOcd9Noph3E,
+  access_token_key: process.env.950342034690400256-EI5VbV98U4uj09c8FADYSOnfHopTAu5,
+  access_token_secret: process.env.JaCHBbJ3LX8DGCbINEuowkwKvNGR4OpuU08BlV29Gx7bS,
+};
+
+exports.spotify = {
+  id: process.env.5eb560a747284ac591811dda4aef6a74,
+  secret: process.env.3fdac09b65664de9be60dc5acf7d5015,
+};
---
+++ liri.js
@@ -0,0 +1,6 @@
+// adds code to read and set the environment variables with the dotenv package:
+
+require("dotenv").config();
+
+var twitter = require();
+var spotify = require();

116702a102dc333cba51189c894731c705d61c55
deliscoding <danelis19@gmail.com>
2018-03-07 10:07:58 -0500

    added the dependencies to liri.js

--- liri.js
+++ liri.js
@@ -1,6 +1,22 @@
-// adds code to read and set the environment variables with the dotenv package:
+//Dependencies
 
+// adds code to read and set the environment variables with the dotenv package:
 require("dotenv").config();
 
-var twitter = require();
-var spotify = require();
+//import twitter npm package
+var twitter = require("twitter");
+
+//imports the node spotify npm package
+var spotify = require("node-spotify-api");
+
+//imports the api keys
+var keys = require("./keys");
+
+//imports the request npm package.
+var request = require("request");
+
+// Import the FS package for read/write.
+var fs = require("fs");
+
+// Start up the spotify API client using our client id and secret
+var spotify = new Spotify(keys.spotify);
